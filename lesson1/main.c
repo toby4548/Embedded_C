c912c1900368fd12210f9398631f9a7912f92511
Tony Huang <Tony Huang>
2018-03-23 23:00:40 +0100

    Finish Lesson 1

---
+++ lesson1/main.c
@@ -0,0 +1,21 @@
+
+int main() {
+    unsigned int counter = 0;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    
+    return 0;
+}
