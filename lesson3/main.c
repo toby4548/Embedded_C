2641bea3f5d00ceec39668f73ad6814c3901c453
Tony Huang <toby4548@gmail.com>
2018-03-28 21:13:17 +0200

    Initial Lesson3

---
+++ lesson3/main.c
@@ -0,0 +1,14 @@
+
+int main() {
+    int counter = 0;
+    while (counter<21) {
+        ++counter;
+        if (counter & 1 != 0) {
+          //do something
+        }
+        else {
+          //do something else
+        }
+    }
+    return 0;
+}
