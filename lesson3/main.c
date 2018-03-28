6dfd221264667c2806861f34af1e67c66c02f1ba
Tony Huang <toby4548@gmail.com>
2018-03-28 21:32:23 +0200

    Finish Lesson 3

--- lesson3/main.c
+++ lesson3/main.c
@@ -1,14 +1,13 @@
-
+int counter = 0;
 int main() {
-    int counter = 0;
-    while (counter<21) {
-        ++counter;
-        if (counter & 1 != 0) {
-          //do something
-        }
-        else {
-          //do something else
-        }
+    int *p_int = &counter;
+    while (*p_int<21) {
+        
+        ++*p_int;
+
     }
+    p_int = (int *)0x20000002U;
+    *p_int = 0xDEADBEEF;
+    
     return 0;
 }
