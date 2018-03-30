2ded8f6d32b81ac4ba653f39b429d8e0f36ad524
Tony Huang <toby4548@gmail.com>
2018-03-30 23:46:39 +0200

    Finish lesson 6

--- lesson6/main.c
+++ lesson6/main.c
@@ -1,22 +1,48 @@
 #include "lm4f120h5qr.h"
 
+#define LED_RED   (1U<<1)
+#define LED_BLUE  (1U<<2)
+#define LED_GREEN (1U<<3)
+
 int main() {
-    SYSCTL_RCGCGPIO_R = 0x20U; //Turn off Port F clock gating
-    GPIO_PORTF_DIR_R = 0x0EU; //set pin to output
-    GPIO_PORTF_DEN_R = 0x0EU; //set output type to digital output
+    /*
+    unsigned int a = 0x5A5A5A5A;
+    unsigned int b = 0xDEADBEEF;
+    unsigned int c;
     
+    
+    c = a|b; //OR
+    c = a&b; //AND
+    c = a^b; //exclusice OR
+    c = ~b; //NOT
+    c = b>>1; //right-shift
+    c = b<<3; //left-shift
+  
+    int x =1024;
+    int y = -1024;
+    int z;
+    
+    z = x>>3;
+    z = y>>3;
+    */ 
+   
+    SYSCTL_RCGCGPIO_R |= (1U<<5); //Turn off Port F clock gating
+    GPIO_PORTF_DIR_R |= (LED_RED|LED_BLUE|LED_GREEN); //set pin to output
+    GPIO_PORTF_DEN_R |= (LED_RED|LED_BLUE|LED_GREEN); //set output type to digital output
+    
+    GPIO_PORTF_DATA_R |= LED_BLUE;//turn the blue LED on
     while (1) {
         int volatile counter = 0;
         while (counter<1000000) {
             counter++;
         }
-        GPIO_PORTF_DATA_R = 0x02U; //set GPIO data to turn on the LED
+        GPIO_PORTF_DATA_R |= LED_RED; //set GPIO data to turn on the LED
         
         counter = 0;
         while (counter<1000000) {
             counter++;
         }
-        GPIO_PORTF_DATA_R = 0x00U; //set GPIO data to turn off the LED
+        GPIO_PORTF_DATA_R &= ~LED_RED; //set GPIO data to turn off the LED
     }
     
     return 0;
