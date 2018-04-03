66a7df16f10b1572b2c54a6f5633823dba022678
Tony Huang <toby4548@gmail.com>
2018-04-03 19:28:39 +0200

    Initial Lesson 9

---
+++ lesson9/main.c
@@ -0,0 +1,37 @@
+#include "lm4f120h5qr.h"
+
+#define LED_RED   (1U<<1)
+#define LED_BLUE  (1U<<2)
+#define LED_GREEN (1U<<3)
+
+void delay(int iter) {
+    int volatile counter = 0;
+    while (counter<iter) {
+            counter++;
+        }
+}
+
+int main() {
+   
+    SYSCTL_RCGCGPIO_R |= (1U<<5); //Turn off Port F clock gating
+    SYSCTL_GPIOHBCTL_R |= (1U<<5); //enable AHB for GPIO
+    GPIO_PORTF_AHB_DIR_R |= (LED_RED|LED_BLUE|LED_GREEN); //set pin to output
+    GPIO_PORTF_AHB_DEN_R |= (LED_RED|LED_BLUE|LED_GREEN); //set output type to digital output
+    
+    GPIO_PORTF_AHB_DATA_BITS_R[LED_BLUE]= LED_BLUE;//turn the blue LED on
+    while (1) {
+        //GPIO_PORTF_DATA_R |= LED_RED; //set GPIO data to turn on the LED
+        //*((unsigned long volatile *)(0x40025000+(LED_RED<<2))) = LED_RED;
+        //*(GPIO_PORTF_DATA_BITS_R + LED_RED) = LED_RED;
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = LED_RED;
+        
+      
+        delay(1000000);
+        
+        GPIO_PORTF_AHB_DATA_BITS_R[LED_RED] = 0; //set GPIO data to turn off the LED
+        
+        delay(500000);
+    }
+    
+    return 0;
+}
