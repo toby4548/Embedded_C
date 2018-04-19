ab7c5561a311ad79cef13ce37f4307d7d8dc19db
Tony Huang <toby4548@gmail.com>
2018-04-19 13:01:03 +0200

    Finish first time Lesson 17

--- lesson17/main.c
+++ lesson17/main.c
@@ -1,22 +1,26 @@
 #include <stdint.h>
-#include "tm4c_cmsis.h"
+#include "TM4C123GH6PM.h"
 #include "bsp.h"
 #include "intrinsics.h"
 
 int main() {
     
     SYSCTL->RCGC2 |= (1U<<5); //Turn off Port F clock gating
-    SYSCTL->GPIOHSCTL |= (1U<<5); //enable AHB for GPIO
-    GPIOF_HS->DIR |= (LED_RED|LED_BLUE|LED_GREEN); //set pin to output
-    GPIOF_HS->DEN |= (LED_RED|LED_BLUE|LED_GREEN); //set output type to digital output
+    SYSCTL->GPIOHBCTL |= (1U<<5); //enable AHB for GPIO
+    GPIOF_AHB->DIR |= (LED_RED|LED_BLUE|LED_GREEN); //set pin to output
+    GPIOF_AHB->DEN |= (LED_RED|LED_BLUE|LED_GREEN); //set output type to digital output
     
     SysTick->LOAD = SYS_CLOCK_HZ/2U - 1U;
     SysTick->VAL = 0U;
     SysTick->CTRL = (1U<<2) | (1U<<1) | (1U);
     
+    SysTick_Handler();
+    
     __enable_interrupt();
     //GPIOF_HS->DATA_Bits[LED_BLUE]= 0;//turn the blue LED on
     while (1) {      
+        GPIOF_AHB->DATA_Bits[LED_GREEN] = LED_GREEN;
+        GPIOF_AHB->DATA_Bits[LED_GREEN] = 0U;
     }
     
     //return 0;
