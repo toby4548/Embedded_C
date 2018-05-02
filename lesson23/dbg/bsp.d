e2507af50954982472d1904a2f3b1936f801ed35
Tony Huang <toby4548@gmail.com>
2018-05-02 14:10:54 +0200

    Finish lesson 23 with non-compilable bug

---
+++ lesson23/dbg/bsp.d
@@ -0,0 +1,11 @@
+.\dbg\bsp.o: bsp.c
+.\dbg\bsp.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\dbg\bsp.o: bsp.h
+.\dbg\bsp.o: miros.h
+.\dbg\bsp.o: ..\qpc\3rd_party\ek-tm4c123gxl\TM4C123GH6PM.h
+.\dbg\bsp.o: ..\qpc\3rd_party\CMSIS\Include\core_cm4.h
+.\dbg\bsp.o: ..\qpc\3rd_party\CMSIS\Include\cmsis_version.h
+.\dbg\bsp.o: ..\qpc\3rd_party\CMSIS\Include\cmsis_compiler.h
+.\dbg\bsp.o: ..\qpc\3rd_party\CMSIS\Include\cmsis_armcc.h
+.\dbg\bsp.o: ..\qpc\3rd_party\CMSIS\Include\mpu_armv7.h
+.\dbg\bsp.o: ..\qpc\3rd_party\ek-tm4c123gxl\system_TM4C123GH6PM.h
---
+++ lesson23/dbg/main.d
@@ -0,0 +1,4 @@
+.\dbg\main.o: main.c
+.\dbg\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\dbg\main.o: bsp.h
+.\dbg\main.o: miros.h
